2095aaf334865120559597b9d6c4acf1cd259979
unknown <wnddkdpcqkd@gmail.com>
2021-10-12 18:15:02 +0900

    add backPressHandler

--- src/screen/childInfo/EnterChildInfo.tsx
+++ src/screen/childInfo/EnterChildInfo.tsx
@@ -1,210 +1,214 @@
-import React from 'react'
-import { View, StyleSheet, Text, TouchableOpacity, ScrollView, TextInput, Platform} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { GLOBAL_MARGIN_HORIZON, MAIN_COLOR, SIZE_HEIGHT, SIZE_WIDTH } from '../common/constants';
-import Icon from 'react-native-vector-icons/Ionicons'
-import { StackNavigationProp } from '@react-navigation/stack';
-import { stackInterface } from '../../types/navigationParam';
-import { Divider } from '../common/divider';
-import { TextInputView } from './components/TextInputView';
-import { TextView } from './components/TextView';
-import { Button } from '../common/components/Button';
-import Modal from 'react-native-modal'
-import { DateScroller } from './components/DateScroller';
-import { DiagSelector } from './components/DiagSelector';
-
+import React from 'react';
+import {
+    View,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    ScrollView,
+    TextInput,
+    Platform,
+	BackHandler,
+} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {
+    GLOBAL_MARGIN_HORIZON,
+    MAIN_COLOR,
+    SIZE_HEIGHT,
+    SIZE_WIDTH,
+} from '../common/constants';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {stackInterface} from '../../types/navigationParam';
+import {Divider} from '../common/divider';
+import {TextInputView} from './components/TextInputView';
+import {TextView} from './components/TextView';
+import {Button} from '../common/components/Button';
+import Modal from 'react-native-modal';
+import {DateScroller} from './components/DateScroller';
+import {DiagSelector} from './components/DiagSelector';
 
 interface enterChildInfoProps {
-	navigation : StackNavigationProp<stackInterface,'SocialLogin'>;
+    navigation: StackNavigationProp<stackInterface, 'SocialLogin'>;
 }
 
-export function EnterChildInfo(props : enterChildInfoProps) {
-		
-	// todoList check right info //
+export function EnterChildInfo(props: enterChildInfoProps) {
+    // todoList check right info //
+    const [name, setName] = React.useState<string>();
+    const [birthDate, setBirthDate] = React.useState<Date>();
+    const [sex, setSex] = React.useState<'M' | 'W'>();
+    const [diagnosis, setDiagnosis] = React.useState<string>();
+    const [directInputDiag, setDirectInputDiag] = React.useState<string>();
+    const [weekNum, setWeekNum] = React.useState<number>();
+    const [dateNum, setdateNum] = React.useState<number>();
+    const [height, setHeight] = React.useState<number>();
+    const [weight, setWeight] = React.useState<number>();
 
+    //modal visible
+    const [dateModalVisible, setdateModalVisible] = React.useState<boolean>(false);
+    const [diagModalVisible, setDiagModalVisible] = React.useState<boolean>(false);
 
-	const [name, setName] = React.useState<string>();
-	const [birthDate, setBirthDate] = React.useState<Date>();
-	const [sex, setSex] = React.useState<'M'|'W'>();
-	const [diagnosis, setDiagnosis] = React.useState<string>();
-	const [directInputDiag, setDirectInputDiag] = React.useState<string>();
-	const [weekNum,setWeekNum] = React.useState<number>();
-	const [dateNum,setdateNum] = React.useState<number>();
-	const [height, setHeight] = React.useState<number>();
-	const [weight, setWeight] = React.useState<number>();
-	
+    // 필수항목 체크
+    const essentialOptionCheck = (): boolean => {
+        if (name && birthDate && sex && weekNum && dateNum && height && weight) {
+            if (diagnosis == '직접입력') {
+                if (directInputDiag) {
+                    return true;
+                }
+            } else if (diagnosis != undefined) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    };
 
-	//modal visible
-	const [dateModalVisible, setdateModalVisible] = React.useState<boolean>(false);
-	const [diagModalVisible, setDiagModalVisible] = React.useState<boolean>(false);
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.innerContainer}>
+                {Platform.OS == 'android' ? (
+                    <Divider height={GLOBAL_MARGIN_HORIZON} color="white" />
+                ) : null}
+                {/* <TouchableOpacity
+                    onPress={() => props.navigation.reset({routes: [{name: 'Home'}]})}>
+                    <Icon name="arrow-back" style={{fontSize: 30}}></Icon>
+                </TouchableOpacity> */}
 
+                <View style={styles.headerTextContainer}>
+                    <Text style={styles.headerTextStyle}>우리아이등록</Text>
+                </View>
 
-	// 필수항목 체크
-	const essentialOptionCheck = () : boolean => {
-		if(name && birthDate && sex && weekNum && dateNum && height && weight){
-			if(diagnosis == "직접입력"){
-				if(directInputDiag){
-					return true;
-				}
-			} else if(diagnosis != undefined){
-				return true;
-			} else {
-				return false;
-			}
-		}
+                <ScrollView style={{flex: 1}} showsVerticalScrollIndicator={false}>
+                    <TextView text="이름" />
+                    <TextInputView
+                        placeholder={'우리 아이 이름을 한글로 입력해주세요. ex) 김키블'}
+                        style={styles.commonMargin}
+                        onChangeText={setName}></TextInputView>
 
-		return false
-	}
+                    <TextView text="출생일" />
+                    <TextInputView
+                        placeholder={'출생일을 선택해주세요'}
+                        style={styles.commonMargin}
+                        icon="calendar"
+                        editable={false}
+                        iconOnPress={setdateModalVisible}
+                        value={birthDate}></TextInputView>
 
-	return (
-		<SafeAreaView style={styles.container}>
-			<View style={styles.innerContainer}>
-				{
-				    Platform.OS =='android' ? <Divider height={GLOBAL_MARGIN_HORIZON} color="white" /> : null
-			    }
-				<TouchableOpacity onPress={() => props.navigation.reset({routes : [{name : 'Home'}]})}>
-					<Icon name="arrow-back" style={{fontSize : 30}}></Icon>
-				</TouchableOpacity>
-				
-				<View style={{marginTop : SIZE_HEIGHT * 0.03 , marginBottom : SIZE_HEIGHT * 0.1}}>
-						<Text style={styles.headerTextStyle}>우리아이등록</Text>
-				</View>
+                    {/* 성별 */}
+                    <View style={styles.commonMargin}>
+                        <View style={styles.sexTextContainer}>
+                            <Text style={styles.text}>성별 </Text>
+                            <Text style={{color: MAIN_COLOR}}>*</Text>
+                        </View>
+                        <View style={styles.buttonSexContainer}>
+                            <TouchableOpacity
+                                style={[
+                                    styles.buttonSex,
+                                    {borderColor: sex == 'M' ? MAIN_COLOR : '#ededed'},
+                                ]}
+                                onPress={() => setSex('M')}>
+                                <Text style={{color: sex == 'M' ? MAIN_COLOR : '#aaaaaa'}}>
+                                    남
+                                </Text>
+                            </TouchableOpacity>
 
-				<ScrollView 
-					style={{flex : 1}}
-					showsVerticalScrollIndicator={false}
-				>
-					
-					<TextView text='이름'/>
-					<TextInputView 
-					placeholder={'우리 아이 이름을 한글로 입력해주세요. ex) 김키블'}
-					style={{marginBottom : SIZE_HEIGHT * 0.06}}
-					onChangeText={setName}
-					></TextInputView>
+                            <TouchableOpacity
+                                style={[
+                                    styles.buttonSex,
+                                    {borderColor: sex == 'W' ? MAIN_COLOR : '#ededed'},
+                                ]}
+                                onPress={() => setSex('W')}>
+                                <Text style={{color: sex == 'W' ? MAIN_COLOR : '#aaaaaa'}}>
+                                    여
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
 
-					<TextView text='출생일'/>
-					<TextInputView 
-					placeholder={'출생일을 선택해주세요'}
-					style={{marginBottom : SIZE_HEIGHT * 0.06}}
-					icon="calendar"
-					editable={false}
-					iconOnPress={setdateModalVisible}
-					value={birthDate}
-					></TextInputView>
+                    <TextView text="진단명" />
+                    <TextInputView
+                        placeholder={'진단명을 선택해 주세요'}
+                        style={styles.commonMargin}
+                        icon="chevron-down"
+                        iconOnPress={setDiagModalVisible}
+                        editable={false}
+                        value={diagnosis}></TextInputView>
+                    {diagnosis === '직접입력' ? (
+                        <TextInputView
+                            placeholder={'진단명을 입력해 주세요'}
+                            style={styles.diagnosisTextInput}
+                            value={directInputDiag}
+                            onChangeText={setDirectInputDiag}></TextInputView>
+                    ) : null}
+                    {/* 출생시 주수 */}
+                    <TextView text="출생시 주수" />
+                    <View style={styles.weekContainer}>
+                        <TextInputView
+                            placeholder={'ex) 23'}
+                            style={styles.weekTextInput}
+                            unitText="주"
+                            keyboardType="numeric"
+                            onChangeText={setWeekNum}></TextInputView>
+                        <TextInputView
+                            placeholder={'ex) 10'}
+                            style={styles.weekTextInput}
+                            unitText="일"
+                            keyboardType="numeric"
+                            onChangeText={setdateNum}></TextInputView>
+                    </View>
 
-					{/* 성별 */}
-					<View style={{marginBottom : SIZE_HEIGHT * 0.06}}>
-						<View style={{flexDirection: 'row', marginBottom : SIZE_HEIGHT * 0.02}}>
-							<Text style={styles.text}>성별 </Text>
-							<Text style={{color: MAIN_COLOR}}>*</Text>
-						</View>
-						<View style={{flexDirection : 'row', justifyContent : 'space-between'}}>
-							<TouchableOpacity style={[styles.buttonSex,{borderColor : sex == 'M' ? MAIN_COLOR : '#ededed'}]}
-							onPress={() => setSex('M')}
-							>
-								<Text style={{color : sex == 'M' ? MAIN_COLOR : '#aaaaaa'}}>남</Text>
-							</TouchableOpacity>
+                    <TextView text="키" />
+                    <TextInputView
+                        placeholder={'키를 입력해주세요'}
+                        style={styles.commonMargin}
+                        unitText="cm"
+                        keyboardType="numeric"
+                        onChangeText={setHeight}></TextInputView>
 
-							<TouchableOpacity style={[styles.buttonSex,{borderColor : sex == 'W' ? MAIN_COLOR : '#ededed'}]}
-							onPress={() => setSex('W')}
-							>
-								<Text style={{color : sex == 'W' ? MAIN_COLOR : '#aaaaaa'}}>여</Text>
-							</TouchableOpacity>
-						</View>
-					</View>
+                    <TextView text="몸무게" />
+                    <TextInputView
+                        placeholder={'몸무게를 입력해주세요'}
+                        style={styles.weightTextInput}
+                        unitText="kg"
+                        keyboardType="numeric"
+                        onChangeText={setWeight}></TextInputView>
 
-					<TextView text='진단명'/>
-					<TextInputView 
-					placeholder={'진단명을 선택해 주세요'}
-					style={{marginBottom : SIZE_HEIGHT * 0.06}}
-					icon="chevron-down"
-					iconOnPress={setDiagModalVisible}
-					editable={false}
-					value={diagnosis}
-					></TextInputView>
-					{
-						diagnosis === "직접입력"
-						?
-						<TextInputView 
-						placeholder={'진단명을 입력해 주세요'}
-						style={{marginBottom : SIZE_HEIGHT * 0.06 , marginTop : - SIZE_HEIGHT * 0.05 }}
-						value={directInputDiag}
-						onChangeText={setDirectInputDiag}
-						></TextInputView>
-						: null
-					}
-					{/* 출생시 주수 */}
-					<TextView text='출생시 주수'/>
-					<View style={{flexDirection : 'row', justifyContent : 'space-between'}}>
-						<TextInputView 
-						placeholder={'ex) 23'}
-						style={{marginBottom : SIZE_HEIGHT * 0.06, width : SIZE_WIDTH * 0.40}}
-						unitText="주"
-						keyboardType='numeric'
-						onChangeText={setWeekNum}
-						></TextInputView>
-						<TextInputView 
-						placeholder={'ex) 10'}
-						style={{marginBottom : SIZE_HEIGHT * 0.06, width : SIZE_WIDTH * 0.40}}
-						unitText="일"
-						keyboardType='numeric'
-						onChangeText={setdateNum}
-						></TextInputView>
-					</View>
+                    <Button
+                        text={'다음으로'}
+                        textColor={!essentialOptionCheck() ? '#707070' : '#FFFFFF'}
+                        style={{
+                            backgroundColor: essentialOptionCheck() ? MAIN_COLOR : '#ededed',
+                        }}
+                        onPress={() => {
+                            props.navigation.navigate('NurseryCaution');
+                        }}
+                        disable={!essentialOptionCheck()}
+                    />
+                </ScrollView>
 
-					<TextView text='키'/>
-					<TextInputView 
-					placeholder={'키를 입력해주세요'}
-					style={{marginBottom : SIZE_HEIGHT * 0.06}}
-					unitText="cm"
-					keyboardType='numeric'
-					onChangeText={setHeight}
-					></TextInputView>
+                {/* 출생일 dateScroller */}
+                <Modal isVisible={dateModalVisible}>
+                    <DateScroller
+                        setDate={setBirthDate}
+                        date={birthDate}
+                        setModalVisible={setdateModalVisible}></DateScroller>
+                </Modal>
 
-					<TextView text='몸무게'/>
-					<TextInputView 
-					placeholder={'몸무게를 입력해주세요'}
-					style={{marginBottom : SIZE_HEIGHT * 0.1}}
-					unitText="kg"
-					keyboardType='numeric'
-					onChangeText={setWeight}
-					></TextInputView>
-
-					<Button 
-					text={'다음으로'} 
-					textColor={!essentialOptionCheck() ? '#707070' : '#FFFFFF'} 
-					style={{backgroundColor : essentialOptionCheck() ? MAIN_COLOR : '#ededed' }} 
-					onPress={() => {props.navigation.navigate('NurseryCaution')}}
-                    disable = {!essentialOptionCheck()}
-					/>
-				</ScrollView>
-
-
-				{/* 출생일 dateScroller */}
-				<Modal isVisible={dateModalVisible}>
-					<DateScroller 
-						setDate={setBirthDate} 
-						date={birthDate}
-						setModalVisible={setdateModalVisible}
-					></DateScroller>
-				</Modal>
-
-				<Modal isVisible={diagModalVisible}
-					onBackdropPress={()=>setDiagModalVisible(false)}
-					style={{margin : 0}}
-				>
-					<DiagSelector
-						setModalVisible={setDiagModalVisible}
-						setDiagnosis={setDiagnosis}
-					></DiagSelector>
-				</Modal>
-
-			</View>
-		</SafeAreaView>
-	);
+                <Modal
+                    isVisible={diagModalVisible}
+                    onBackdropPress={() => setDiagModalVisible(false)}
+                    style={{margin: 0}}>
+                    <DiagSelector
+                        setModalVisible={setDiagModalVisible}
+                        setDiagnosis={setDiagnosis}></DiagSelector>
+                </Modal>
+            </View>
+        </SafeAreaView>
+    );
 }
+
 const styles = StyleSheet.create({
-	container: {
+    container: {
         flex: 1,
         backgroundColor: '#ffffff',
     },
@@ -212,24 +216,54 @@ const styles = StyleSheet.create({
         flex: 1,
         marginHorizontal: GLOBAL_MARGIN_HORIZON,
     },
-	headerTextStyle: {
-		fontFamily: 'Pretendard',
-		fontSize: 28,
-		fontWeight: 'bold',
-		color: '#111111',
-	},
-	text : {
-		fontFamily: 'Pretendard',
-		fontSize: 20,
-		color: '#111111',
-	},
-	buttonSex : {
-		width : SIZE_WIDTH * 0.40,
-		paddingVertical : SIZE_HEIGHT * 0.017,
-		borderRadius: 20,
-		// borderColor : '#ededed',
-		borderWidth : 2,
-		justifyContent : 'center', 
-		alignItems : 'center'
-	}
-})
+    headerTextContainer: {
+        marginTop: SIZE_HEIGHT * 0.03,
+        marginBottom: SIZE_HEIGHT * 0.1,
+    },
+    headerTextStyle: {
+        fontFamily: 'Pretendard',
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: '#111111',
+    },
+    text: {
+        fontFamily: 'Pretendard',
+        fontSize: 20,
+        color: '#111111',
+    },
+    commonMargin: {
+        marginBottom: SIZE_HEIGHT * 0.06,
+    },
+    sexTextContainer: {
+        flexDirection: 'row',
+        marginBottom: SIZE_HEIGHT * 0.02,
+    },
+    buttonSexContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    buttonSex: {
+        width: SIZE_WIDTH * 0.4,
+        paddingVertical: SIZE_HEIGHT * 0.017,
+        borderRadius: 20,
+        // borderColor : '#ededed',
+        borderWidth: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    diagnosisTextInput: {
+        marginBottom: SIZE_HEIGHT * 0.06,
+        marginTop: -SIZE_HEIGHT * 0.05,
+    },
+    weekContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    weekTextInput: {
+        marginBottom: SIZE_HEIGHT * 0.06,
+        width: SIZE_WIDTH * 0.4,
+    },
+    weightTextInput: {
+        marginBottom: SIZE_HEIGHT * 0.1,
+    },
+});
--- src/screen/childInfo/components/TextInputView.tsx
+++ src/screen/childInfo/components/TextInputView.tsx
@@ -1,58 +1,74 @@
-import { valueFromAST } from 'graphql';
+import {valueFromAST} from 'graphql';
 import React from 'react';
-import {View, StyleSheet, Text, TextInput, ViewStyle, TouchableOpacity, KeyboardTypeOptions} from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons'
-import { MAIN_COLOR } from '../../common/constants';
-import { getDateYMD } from '../../common/service/dateService';
+import {
+    View,
+    StyleSheet,
+    Text,
+    TextInput,
+    ViewStyle,
+    TouchableOpacity,
+    KeyboardTypeOptions,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {MAIN_COLOR} from '../../common/constants';
+import {getDateYMD} from '../../common/service/dateService';
+
 interface textInputViewProps {
     placeholder: string;
     style?: ViewStyle;
-	icon? : string
-	unitText? : string
-	keyboardType? : KeyboardTypeOptions
-	onChangeText?(value : any) : void
-	editable? : boolean
-	iconOnPress?(value : any) : void
-	value? : string | Date
+    icon?: string;
+    unitText?: string;
+    keyboardType?: KeyboardTypeOptions;
+    onChangeText?(value: any): void;
+    editable?: boolean;
+    iconOnPress?(value: any): void;
+    value?: string | Date;
 }
 
 export function TextInputView(props: textInputViewProps) {
-	
-	let value
-	if(typeof(props.value) == 'string'){
-		value = props.value
-	}else if(typeof(props.value) == 'undefined'){
-		value = undefined
-	}else {
-		value = getDateYMD(props.value,'-')
-	}
+    let value;
+    if (typeof props.value == 'string') {
+        value = props.value;
+    } else if (typeof props.value == 'undefined') {
+        value = undefined;
+    } else {
+        value = getDateYMD(props.value, '-');
+    }
+
     return (
-        <View style={[{borderBottomWidth: 1, borderBottomColor: '#ededed'},props.style]}>
-            <View style={{flexDirection : 'row', justifyContent : 'space-between', alignItems :'center'}}>
-                <TextInput 
-				placeholder={props.placeholder} 
-				keyboardType={props.keyboardType ? props.keyboardType : 'default'}
-				onChangeText={(text)=> props.onChangeText? props.onChangeText(text) : null}
-				editable={props.editable}
-				value={props.value? value : undefined}
-				style={{color : 'black'}}
-				></TextInput>
-				{
-					props.icon
-					? 		
-					<TouchableOpacity onPress={() => props.iconOnPress? props.iconOnPress(true) : null}>
-						<Icon name={props.icon} style={{fontSize: 25, color : MAIN_COLOR}}></Icon>
-					</TouchableOpacity>
-					: null
-				}
-				{
-					props.unitText
-					?
-					<View>
-						<Text style={{fontSize : 20, color : '#aaaaaa', fontWeight : 'bold'}}>{props.unitText}</Text>
-					</View>
-					: null
-				}
+        <View
+            style={[
+                {borderBottomWidth: 1, borderBottomColor: '#ededed'},
+                props.style,
+            ]}>
+            <View
+                style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                }}>
+                <TextInput
+                    placeholder={props.placeholder}
+                    keyboardType={props.keyboardType ? props.keyboardType : 'default'}
+                    onChangeText={text =>
+                        props.onChangeText ? props.onChangeText(text) : null
+                    }
+                    editable={props.editable}
+                    value={props.value ? value : undefined}
+                    style={{color: 'black', flex: 1}}></TextInput>
+                {props.icon ? (
+                    <TouchableOpacity
+                        onPress={() =>
+                            props.iconOnPress ? props.iconOnPress(true) : null
+                        }>
+                        <Icon name={props.icon} style={styles.icon}></Icon>
+                    </TouchableOpacity>
+                ) : null}
+                {props.unitText ? (
+                    <View>
+                        <Text style={styles.unitText}>{props.unitText}</Text>
+                    </View>
+                ) : null}
             </View>
         </View>
     );
@@ -62,5 +78,14 @@ const styles = StyleSheet.create({
         fontFamily: 'Pretendard',
         fontSize: 20,
         color: '#111111',
+    },
+    icon: {
+        fontSize: 25,
+        color: MAIN_COLOR,
+    },
+    unitText: {
+        fontSize: 20,
+        color: '#aaaaaa',
+        fontWeight: 'bold',
     },
 });
