3cdcdda61205f4f43a211786ae3d7b384ca27ac7
goddls123 <coolch0916@gmail.com>
2021-11-01 21:54:14 +0900

    add time table & schedule design

--- src/screen/common/constants.tsx
+++ src/screen/common/constants.tsx
@@ -1,15 +1,17 @@
-import { Dimensions } from 'react-native'
+import {Dimensions} from 'react-native';
 
 export const SIZE_WIDTH = Dimensions.get('window').width;
 export const SIZE_HEIGHT = Dimensions.get('window').height;
 export const GLOBAL_MARGIN_HORIZON = SIZE_WIDTH * 0.05;
 export const GLOBAL_MARGIN_VERTICAL = SIZE_HEIGHT * 0.05;
 export const ANDROID_KEYS = {
-    kConsumerKey: "QuWkmldDj4pP3aPAq59I",
-    kConsumerSecret: "6RQek41Dcj",
-    kServiceAppName: "키블"  
-}
+  kConsumerKey: 'QuWkmldDj4pP3aPAq59I',
+  kConsumerSecret: '6RQek41Dcj',
+  kServiceAppName: '키블',
+};
 
 export const MAIN_COLOR = '#ff8a5c';
 export const FONT_COLOR_BLACK = '#111111';
-export const PLACE_HOLDER_COLOR = "#d5d5d5"
+export const PLACE_HOLDER_COLOR = '#d5d5d5';
+export const GREY_BACKGOUND_COLOR = '#f6f6f6';
+export const GREY_BORDER_COLOR = '#ededed';
--- src/types/calendarTypes.ts
+++ src/types/calendarTypes.ts
@@ -1,41 +1,41 @@
 export interface schedule {
-    schedule_id : number,
-    user_id : number,
-    schedule_date : string,
-    title : string,
-    location : string,
-    memo : string,
-    color : string,
-    theraphist_name? : string,
-    start_time : any,
-    end_time : any,
-    notify_time : any,
-    repeat_cycle : string
+  schedule_id: number;
+  user_id: number;
+  schedule_date: string;
+  title: string;
+  location: string;
+  memo: string;
+  color: string;
+  theraphist_name?: string;
+  start_time: any;
+  end_time: any;
+  notify_time: any;
+  repeat_cycle: string;
 }
 
-export interface weekInfo{
-    year : String,
-    month : String,
-    date : String,
-    day : Number,
-    kDay : String
+export interface weekInfo {
+  year: String;
+  month: String;
+  date: String;
+  day: Number;
+  kDay: String;
 }
 
 export interface scheduleFront {
-    schedule_date : string,
-    title : string,
-    location : string,
-    memo : string,
-    color : string,
-    theraphist_name? : string,
-    start_time : any,
-    end_time : any,
+  schedule_date: string;
+  title: string;
+  location: string;
+  memo: string;
+  color: string;
+  theraphist_name?: string;
+  start_time: any;
+  end_time: any;
 }
 
 export interface DateObject {
-    day: number;
-    dateString: string;
-    month: number;
-    timestamp: number;
-    year: number;
+  day: number;
+  dateString: string;
+  month: number;
+  timestamp: number;
+  year: number;
 }
